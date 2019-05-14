f45f6870713b2cd232df1d2abe3daa608d38b6a0
mllsumulong <mllsumulong@gmail.com>
2019-05-14 15:27:37 -0700

    Updates

--- precache-manifest.250ac560c81db7370edca2cf7ea8aa8f.js
+++ precache-manifest.250ac560c81db7370edca2cf7ea8aa8f.js
@@ -12,19 +12,19 @@ self.__precacheManifest = [
     "url": "/Clicky-Game/static/js/runtime~main.fab82da4.js"
   },
   {
-    "revision": "6f55d2a5691edda2bb9c",
-    "url": "/Clicky-Game/static/js/main.6f55d2a5.chunk.js"
+    "revision": "3a5ce3d4ef90a18f5573",
+    "url": "/Clicky-Game/static/js/main.3a5ce3d4.chunk.js"
   },
   {
     "revision": "1744db918d7799a6e6d6",
     "url": "/Clicky-Game/static/js/2.1744db91.chunk.js"
   },
   {
-    "revision": "6f55d2a5691edda2bb9c",
+    "revision": "3a5ce3d4ef90a18f5573",
     "url": "/Clicky-Game/static/css/main.1975f4dc.chunk.css"
   },
   {
-    "revision": "a2d04a6f9293bef90a3ce6116ee431b5",
+    "revision": "2ad6a8c2e557bbd3ecd0d8b5402a4b5e",
     "url": "/Clicky-Game/index.html"
   }
 ];
